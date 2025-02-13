b79e21344e44e76a7c24e575a1f90903893ca945
AiemHao <thienhaocantho@gmail.com>
2025-02-13 23:11:54 +0700

    add backgroud at home

--- fontend/src/client/body/HomePage/HomePage.jsx
+++ fontend/src/client/body/HomePage/HomePage.jsx
@@ -3,6 +3,7 @@ import HeroSection from '../../components/HeroSection/HeroSection.jsx';
 import HList from '../../components/HList/HList.jsx';
 import { useState, useEffect } from 'react';
 import API from '../../../shared/utils/API.js';
+import logo from "../../../shared/assets/logo/logo.svg";
 
 
 function HomePage() {
@@ -11,6 +12,8 @@ function HomePage() {
         <div className={styles.home}>
             <HeroSection />
             <HList/>
+            <img src={logo} alt="" className={styles.leftImg} />
+            <img src={logo} alt="" className={styles.rightImg} /> 
         </div>
     );
 }
